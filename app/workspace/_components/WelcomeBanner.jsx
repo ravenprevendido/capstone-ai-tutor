87584367880fc83c1cec136c3f62a3a6f292bca4
Raven Prevendido <prevendidoraven_bsit@plmun.edu.ph>
2025-10-05 13:22:49 +0800

    capstone4.6percent

--- app/workspace/_components/WelcomeBanner.jsx
+++ app/workspace/_components/WelcomeBanner.jsx
@@ -1,12 +1,23 @@
+"use client";
+
+import { useUser } from '@clerk/nextjs'
 import React from 'react'
 
 function WelcomeBanner() {
-  return (
+
+  const {user} = useUser();
+  
+  const displayName = user?.firstName || user?.username || "Guest";
+
+  return (  
+
         <div className='p-6 bg-gradient-to-br from-green-700 via-green-900 to-green-500 rounded-xl mb-6'>
-            <h2 className='font-bold text-2xl text-white'>Welcome Back, John!</h2>
+            <h2 className='font-bold text-2xl text-white'>Welcome Back, {displayName}</h2>
             <p className='text-white text-sm mt-1'>Here's your Learning progress today keep it up!</p>
         </div>
   )
 }
+
+
 
 export default WelcomeBanner
